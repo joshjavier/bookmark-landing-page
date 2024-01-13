a5cdeae14723774318dbd54de57ee9cdc2d97498
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-01-13 23:48:46 +0800

    Add desktop responsive styles to features section

--- src/components/features-section.tsx
+++ src/components/features-section.tsx
@@ -3,15 +3,17 @@ import { features } from '../data.json';
 
 function FeaturesSection() {
   return (
-    <section className="features" aria-labelledby="features-label">
-      <div className="wrapper">
+    <section className="features region" aria-labelledby="features-label">
+      <div className="intro wrapper">
         <h2 id="features-label">Features</h2>
         <p>
-          Our aim is to make it quick and easy for you to access your favorite
+          Our aim is to make it quick and easy for you to access your favourite
           websites. Your bookmarks sync between your devices so you can access
           them on the go.
         </p>
+      </div>
 
+      <div className="tabs-container wrapper">
         <Tabs features={features} />
       </div>
     </section>
--- src/components/tabs.tsx
+++ src/components/tabs.tsx
@@ -1,15 +1,42 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Feature } from '../definitions';
 
 function Tabs({ features }: { features: Feature[] }) {
   const [active, setActive] = useState(1);
+  const [orientation, setOrientation] = useState<'horizontal' | 'vertical'>(
+    'horizontal',
+  );
+  const tablist = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // Set a threshold at which the tablist switches from vertical to horizontal
+    const threshold = 600;
+    tablist.current?.style.setProperty('--threshold', threshold + 'px');
+
+    const switchOrientation = (entries: ResizeObserverEntry[]) => {
+      const { contentRect } = entries[0];
+      if (contentRect.width < threshold) {
+        setOrientation('vertical');
+      } else {
+        setOrientation('horizontal');
+      }
+    };
+
+    // Update tablist aria-orientation depending on threshold
+    const ro = new ResizeObserver(switchOrientation);
+    ro.observe(tablist.current as HTMLElement);
+
+    return () => ro.disconnect();
+  }, []);
 
   return (
     <div className="tabs">
       <div
         role="tablist"
         aria-labelledby="features-label"
-        aria-orientation="vertical"
+        aria-orientation={orientation}
+        className="switcher"
+        ref={tablist}
       >
         {features.map(({ id, label }) => (
           <button
@@ -30,11 +57,18 @@ function Tabs({ features }: { features: Feature[] }) {
           role="tabpanel"
           id={`fp-${id}`}
           aria-labelledby={`fl-${id}`}
-          hidden={id !== active}
+          aria-hidden={id !== active}
         >
-          <img src={image} alt="" />
-          <h3>{title}</h3>
-          <p>{description}</p>
+          <div className="image bg-blue-pill">
+            <img src={image} alt="" />
+          </div>
+          <div className="text">
+            <h3>{title}</h3>
+            <p>{description}</p>
+            <a href="#" className="btn primary">
+              More Info
+            </a>
+          </div>
         </div>
       ))}
     </div>
