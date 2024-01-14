222e4464d678ca964e033c83ebdc46108df557dc
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-01-14 08:54:24 +0800

    Add desktop responsive styles to the footer

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -5,20 +5,19 @@ import SocialLinks from './social-links';
 function SiteFooter() {
   return (
     <footer className="site-footer">
-      <LogoBookmark />
-      <nav aria-labelledby="footer-label">
-        <h2 id="footer-label" className="visually-hidden">
-          Footer
-        </h2>
-        <ul role="list">
-          {navlinks.map((navlink) => (
-            <li key={navlink}>
-              <a href="#">{navlink}</a>
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <SocialLinks />
+      <div className="inner wrapper">
+        <LogoBookmark />
+        <nav aria-label="Footer">
+          <ul role="list">
+            {navlinks.map((navlink) => (
+              <li key={navlink}>
+                <a href="#">{navlink}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <SocialLinks />
+      </div>
     </footer>
   );
 }
